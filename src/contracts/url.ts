b0559da990a30febcdb97c0a074bfb34d6321503
Ops <ops@21e8.tech>
2023-07-28 14:31:15 +0200

    Add transfer method to Url

--- src/contracts/url.ts
+++ src/contracts/url.ts
@@ -4,6 +4,7 @@ import {
     prop,
     SmartContract,
     hash160,
+    hash256,
     assert,
     ByteString,
     PubKey,
@@ -32,5 +33,21 @@ export class Buyable extends SmartContract {
             `checkSig failed, pubKeyHash: ${this.owner}`
         )
     }
+
+    @method()
+    public transfer(owner: PubKeyHash, signature: Sig, pubkey: PubKey) {
+
+        assert(hash160(pubkey) === this.owner)
+        assert(
+            this.checkSig(signature, pubkey),
+            `checkSig failed, pubKeyHash: ${this.owner}`
+        )
+
+        this.owner = owner
+
+        let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+        assert(this.ctx.hashOutputs == hash256(outputs), 'hashOutputs mismatch')
+    }
+
 }
 
