c7a1535f5dd941bd7e23ef76e8e68ea22498e165
Ops <ops@21e8.tech>
2023-08-06 14:42:03 +0200

    Valid Meeting Contract [Work In Progress]

---
+++ src/contracts/meeting.ts
@@ -0,0 +1,146 @@
+import {
+    assert,
+    ByteString,
+    method,
+    prop,
+    sha256,
+    hash256,
+    Sha256,
+    SmartContract,
+    ScriptContext,
+    HashedSet,
+    PubKey,
+    Sig
+} from 'scrypt-ts'
+
+export class Meeting extends SmartContract {
+
+    @prop(true)
+    admins: HashedSet<PubKey>
+
+    @prop(true)
+    invitees: HashedSet<PubKey>
+
+    @prop(true)
+    attendees: HashedSet<PubKey>
+
+    @prop(true)
+    cancelled: boolean
+
+    @prop(true)
+    inviteRequired: boolean
+
+    constructor(
+      admins: HashedSet<PubKey>,
+      invitees: HashedSet<PubKey>,
+      attendees: HashedSet<PubKey>,
+      inviteRequired: boolean
+    ) {
+        super(...arguments)
+        this.admins = admins
+        this.invitees = invitees
+        this.attendees = attendees
+        this.inviteRequired = inviteRequired
+        this.cancelled = false
+    }
+
+    @method()
+    public uncancel(pubkey: PubKey, sig: Sig) {
+
+        assert(this.checkSig(sig, pubkey),`checkSig failed, pubkey: ${pubkey}`)
+
+        assert(this.admins.has(pubkey))
+
+        this.cancelled = false
+
+        let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+        if (this.changeAmount > 0n) { outputs += this.buildChangeOutput() }
+        assert(this.ctx.hashOutputs == hash256(outputs), 'state not preserved')
+
+    }
+
+    @method()
+    public invite(invitee: PubKey, pubkey: PubKey, sig: Sig) {
+
+        assert(this.checkSig(sig, pubkey),`checkSig failed, pubkey: ${pubkey}`)
+
+        assert(this.admins.has(pubkey))
+
+        if (!this.invitees.has(invitee) && !this.attendees.has(invitee)) {
+          this.invitees.add(invitee)
+        }
+
+        let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+        if (this.changeAmount > 0n) { outputs += this.buildChangeOutput() }
+        assert(this.ctx.hashOutputs == hash256(outputs), 'state not preserved')
+
+    }
+
+    @method()
+    public decline(pubkey: PubKey, sig: Sig) {
+
+      assert(this.checkSig(sig, pubkey),`checkSig failed, pubkey: ${pubkey}`)
+
+      if (this.invitees.has(pubkey)) {
+
+        this.invitees.delete(pubkey)
+
+      }
+
+      if (this.attendees.has(pubkey)) {
+
+        this.attendees.delete(pubkey)
+
+      }
+
+      let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+      if (this.changeAmount > 0n) { outputs += this.buildChangeOutput() }
+      assert(this.ctx.hashOutputs == hash256(outputs), 'state not preserved')
+
+    }
+
+    @method()
+    public attend(pubkey: PubKey, sig: Sig) {
+
+      assert(this.checkSig(sig, pubkey),`checkSig failed, pubkey: ${pubkey}`)
+
+      if (this.inviteRequired) {
+        assert(this.invitees.has(pubkey) || this.admins.has(pubkey))
+      }
+
+      this.attendees.add(pubkey)
+
+      let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+      if (this.changeAmount > 0n) { outputs += this.buildChangeOutput() }
+      assert(this.ctx.hashOutputs == hash256(outputs), 'state not preserved')
+
+    }
+
+    isAttending(pubkey: PubKey): boolean {
+      return this.attendees.has(pubkey)
+    }
+
+    isInvited(pubkey: PubKey): boolean {
+      return this.invitees.has(pubkey)
+    }
+
+    isAdmin(pubkey: PubKey): boolean {
+      return this.admins.has(pubkey)
+    }
+
+    @method()
+    public cancel(pubkey: PubKey, sig: Sig) {
+
+        assert(this.checkSig(sig, pubkey),`checkSig failed, pubkey: ${pubkey}`)
+
+        assert(this.admins.has(pubkey))
+
+        this.cancelled = true
+
+        let outputs: ByteString = this.buildStateOutput(this.ctx.utxo.value)
+        if (this.changeAmount > 0n) { outputs += this.buildChangeOutput() }
+        assert(this.ctx.hashOutputs == hash256(outputs), 'state not preserved')
+
+    }
+
+}
